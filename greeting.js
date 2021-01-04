28f141a9f92cb4458922847860b1efef11449a4a
2019056799 <2019056799@hanyang.ac.kr>
2021-01-05 05:59:13 +0900

    second commit

--- greeting.js
+++ greeting.js
@@ -22,36 +22,51 @@ function askForName(){
     form.addEventListener("submit", handleSubmit);
 }
 
+function deleteName(){
+    localStorage.removeItem(USER_LS);
+    location.reload();
+}
+
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
+
+    const div = document.createElement("div");
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "Back💨";
+    delBtn.classList.add("btn");
+    delBtn.addEventListener("click", deleteName);
+    div.appendChild(delBtn);
+
     if (text == "최유빈") {
         greeting.innerText = `갈매기님 안녕하세요~ 음~ 굳~^^`;
         const image = new Image();
         image.src = `Images/youbin.jpg`;
         image.classList.add('additionalImage');
-        additionalImage.appendChild(image);
+        div.appendChild(image);
     } else if (text == "심은서") {
         greeting.innerText = `얼음요요님 안녕하세요~ 패션이 너무 아름다우세요~ㅇ_<`;
         const image = new Image();
         image.src = `Images/eunsu.jpg`;
         image.classList.add('additionalImage');
-        additionalImage.appendChild(image);
+        div.appendChild(image);
     } else if (text == "권혜진") {
         greeting.innerText = `국내산주스님 안녕하세요~ 토익 공부는 언제쯤..?^^;;`;
         const image = new Image();
         image.src = `Images/hyejin.jpg`;
         image.classList.add('additionalImage');
-        additionalImage.appendChild(image);
+        div.appendChild(image);
     } else if (text == "김주형") {
         greeting.innerText = `전지구적차원(dnd)님 안녕하세요~ 오늘도 즐거운 디앤디하루~ +_+`;
         const image = new Image();
         image.src = `Images/joohyeong.gif`;
         image.classList.add('additionalImage');
-        additionalImage.appendChild(image);
+        div.appendChild(image);
     } else {
         greeting.innerText = `${text}님은 앙팸의 회원이 아닙니다.`
     }
+
+    additionalImage.appendChild(div);
 }
 
 function loadName(){
